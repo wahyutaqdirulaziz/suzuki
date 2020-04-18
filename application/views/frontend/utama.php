923ba9c09e67beb2f998c989d597a30315265343
MOH.FAHRURROZI <56944631+ojik100@users.noreply.github.com>
2020-04-18 00:24:16 -0700

    utama

---
+++ application/views/frontend/utama.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-grid.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/custom.css">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+
+    <nav class="navbar navbar-expand-lg navbar-dark site-header sticky-top py-1 ">
+        <div class="">
+            <img src="assets/img/suzu.png" width="30" height="30" alt="">
+
+            <a class="navbar-brand " href="#"><b>NJS</b></a>
+
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+        <div class="container">
+
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav mr-auto">
+
+                    <a class="nav-item nav-link " href="#">Model A<span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link " href="#">Model B</a>
+                    <a class="nav-item nav-link " href="#">Model C</a>
+                    <a class="nav-item nav-link" href="#">Model D</a>
+                    <a class="nav-item nav-link " href="#">Model F</a>
+
+                    <a class="nav-item nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="#">Features</a>
+
+                </div>
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="#">Features</a>
+                </div>
+
+            </div>
+
+        </div>
+
+    </nav>
+
+<!---crousel-->
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+            <div class="carousel-captions d-none d-md-block">
+                <h5>Second slide label</h5>
+                <a href="" class="btn btn-light btn btn-lg">Custom Mobile</a>
+                <a href="" class="btn btn-outline-light btn btn-lg">Custom Mobile</a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+        </ol>
+
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="assets/img/mobil23.jpg" height="750px" width="100px" class="d-block w-100" alt="...">
+
+            </div>
+            <div class="carousel-item">
+                <img src="assets/img/mobil23.jpg" height="750px" width="200px" class="d-block w-100" alt="...">
+
+            </div>
+            <div class="carousel-item">
+                <img src="assets/img/mobil23.jpg" height="750px" class="d-block w-100" alt="...">
+
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+            <!-- <span class="carousel-control-prev-icon" aria-hidden="true"></span> -->
+            <!-- <span class="sr-only">Previous</span> -->
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+            <!-- <span class="carousel-control-next-icon" aria-hidden="true"></span> -->
+            <!-- <span class="sr-only">Next</span> -->
+        </a>
+    </div>
+
+<!---Penutup crousel-->
+<style type="text/css">
+    .jumbotron{
+      position: relative;
+        background:url(assets/img/tes.jpg)center center;
+        color: #fff;
+         height: 800px;
+        background-size: cover;
+        overflow: hidden;
+        padding: 0%;
+        margin: auto;
+        }.jumbotron1{
+      position: relative;
+        background:url(assets/img/mobil1.jpg)center center;
+        color: #fff;
+         height: 800px;
+        background-size: cover;
+        overflow: hidden;
+    }
+    .jumbotron2{
+      position: relative;
+        background:url(assets/img/mobil.jpg)center center;
+        color: #fff;
+         height: 800px;
+        background-size: cover;
+        overflow: hidden;
+    }
+</style>
+<div id="">
+    <div class="jumbotron">
+        <div class="container-fluid">
+            <br><br><br><br><br><br><br><br><br><br><br><br>
+         <center><h1>Model 3</h1>
+         
+            <a href="" class="btn btn-light btn btn-lg">Custom Mobile</a>
+            <a href="" class="btn btn-outline-light btn btn-lg">Custom Mobile</a>
+         </p>
+         </center>
+       </div>
+    
+</div>
+
+<div id="">
+    <div class="jumbotron1">
+        <div class="container-fluid">
+            <br><br><br><br><br><br><br><br><br><br><br><br>
+         <center><h1>Model 4</h1>
+         
+            <a href="" class="btn btn-light btn btn-lg">Custom Mobile</a>
+            <a href="" class="btn btn-outline-light btn btn-lg">Custom Mobile</a>
+         </p>
+         </center>
+       </div>
+    
+</div>
+
+<div id="">
+    <div class="jumbotron2">
+        <div class="container-fluid">
+            <br><br><br><br><br><br><br><br><br><br><br><br>
+         <center><h1>Model 5</h1>
+         
+            <a href="" class="btn btn-light btn btn-lg">Custom Mobile</a>
+            <a href="" class="btn btn-outline-light btn btn-lg">Custom Mobile</a>
+         </p>
+         </center>
+       </div>
+    
+</div>
+
+
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.js"></script>
+</body>
+
+</html>
