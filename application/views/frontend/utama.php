4088a64257bb561cc893035081f3eddb138a23ac
MOH.FAHRURROZI <56944631+ojik100@users.noreply.github.com>
2020-04-18 00:32:48 -0700

    ojik

--- application/views/frontend/utama.php
+++ application/views/frontend/utama.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/bootstrap-grid.css">
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
-    <link rel="stylesheet" href="assets/css/custom.css">
+    <link rel="stylesheet" href="<?= base_url()?>assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= base_url()?>assets/css/bootstrap-grid.css">
+    <link rel="stylesheet" href="<?= base_url()?>assets/css/bootstrap.css">
+    <link rel="stylesheet" href="<?= base_url()?>assets/css/custom.css">
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
 </head>
 
@@ -170,9 +170,9 @@
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="assets/js/bootstrap.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/bootstrap.bundle.js"></script>
+    <script src="<?= base_url()?>assets/js/bootstrap.js"></script>
+    <script src="<?= base_url()?>assets/js/bootstrap.min.js"></script>
+    <script src="<?= base_url()?>assets/js/bootstrap.bundle.js"></script>
 </body>
 
 </html>
