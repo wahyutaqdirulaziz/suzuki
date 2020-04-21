e9c5a82ee5dd6595e19617b0ca7ad33c460a4748
Wahyudevlombok <44743502+wahyutaqdirulaziz@users.noreply.github.com>
2020-04-21 09:59:33 -0700

    untuk route default cukup di Home dulu

---
+++ application/controllers/Home.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+class Home extends CI_Controller{
+
+// public function __construct()
+// {
+//     parent::__construct();
+//     //Do your magic here
+// }
+
+
+public function index(){
+    $this->load->view('frontend/utama');
+}
+
+public function register() {
+    $this->load->view('frontend/login');
+}
+}
